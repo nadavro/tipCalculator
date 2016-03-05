82bf36c8c5867e6d27782295312eb98aee2771a1
unknown <nadavrozen23@gmail.com>
2016-03-05 17:26:18 +0200

    "change files"

--- app/src/main/java/com/nadavrozen/tipcalculator/MainActivity.java
+++ app/src/main/java/com/nadavrozen/tipcalculator/MainActivity.java
@@ -3,19 +3,20 @@ package com.nadavrozen.tipcalculator;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-//import android.widget.Button;
-//import android.widget.EditText;
-import android.widget.*;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.CheckBox;
+//import android.widget.*;
 import android.view.View.OnClickListener;
 import java.lang.Math;
-import android.app.AlertDialog;
-import android.content.DialogInterface;
+
 
 
 public class MainActivity extends AppCompatActivity {
     TextView tvOut;
     Button btn;
-   // Button btnCancel;
+
 
 
     @Override
@@ -23,24 +24,25 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        tvOut = (TextView) findViewById(R.id.textView2);
-        btn = (Button) findViewById(R.id.button);
-       // btnCancel = (Button) findViewById(R.id.btnCancel);
+        tvOut = (TextView) findViewById(R.id.txtTipResult);
+        btn = (Button) findViewById(R.id.btnCalculate);
+
 
         // create click listener
         OnClickListener oclBtnOk = new OnClickListener() {
 
             @Override
             public void onClick(View v) {
-                EditText billStr = (EditText)findViewById(R.id.editText1);
+                tvOut.setText("");
+                EditText billStr = (EditText)findViewById(R.id.edtBillAmount);
                 String st = billStr.getText().toString();
                 if (!st.isEmpty()){
-                    CheckBox checkBox = (CheckBox)findViewById(R.id.checkBox1);
+                    CheckBox checkBox = (CheckBox)findViewById(R.id.chkRound);
                     //EditText billStr = (EditText)findViewById(R.id.editText1);
                     double billNumbered = Double.parseDouble(st);
                     if(checkBox.isChecked()){
                         //tvOut.setText("checkbox is checked");
-                        int a = 1;
+
                         int rounded = (int) Math.ceil((billNumbered*12)/100);
                         tvOut.setText(rounded+"$");
 
